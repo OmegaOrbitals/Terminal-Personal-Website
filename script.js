87aa00ead274d10b762bbc9c6f111dd85b9ac1b5
OmegaOrbitals <96728724+OmegaOrbitals@users.noreply.github.com>
2024-06-27 09:55:10 +0000

    Added commands and fixed some bugs

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const input = document.querySelector("#input");
-const textElem = document.querySelector("#text")
+const textElem = document.querySelector("#text");
+
+let keys = {
+  shift: false
+}
 
 function output(text) {
   for(let line of text.split("\n")) {
-    let p = document.createElement("p");
-    p.innerText = line;
-    textElem.appendChild(p);
+    let lineElem = document.createElement((line.split("/")[0]) ? line.split("/")[0] : "p");
+    lineElem.innerText = line;
+    textElem.appendChild(lineElem);
   }
 }
 
@@ -17,17 +21,36 @@ function changeInputSize() {
 changeInputSize();
 
 document.addEventListener("keydown", (ev) => {
-  console.log(ev)
+  input.focus();
   if(ev.key == "Enter") {
-    if(["help", "ls"].includes(input.value)) {
-      output(`help - lists commands`);
+    if(keys.shift == false) {
+      ev.preventDefault();
+      output(input.value);
+      if(["help", "ls"].includes(input.value)) {
+        output(`h1/Help\np/${input.value} - lists commands`);
+      }
       input.value = "";
+      changeInputSize();
     }
+  }
+  if(ev.key == "Shift") {
+    keys.shift = true;
+  }
+})
+
+document.addEventListener("keyup", (ev) => {
+  if(ev.key == "Shift") {
+    keys.shift = false;
   }
 })
 
 input.addEventListener("input", (ev) => {
   changeInputSize();
+})
+
+document.body.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  input.focus();
 })
 
 document.body.addEventListener("touchend", (ev) => {
